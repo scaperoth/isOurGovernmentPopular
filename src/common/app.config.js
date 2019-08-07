a6144486200e50513de9feb0a5782b628d566e35
Matthew Scaperoth <scaperoth@gmail.com>
2019-08-07 12:23:57 -0500

    fixng typo found in env comparison

--- src/common/app.config.js
+++ src/common/app.config.js
@@ -1,8 +1,8 @@
 // app config with environment variables
 
 let apiUrl = process.env.GOVT_SENT_API_URL || 'http://localhost:5000';
-console.log('process.env', process.env);
-if(process.env.NODE_ENV === 'producution') {
+
+if(process.env.NODE_ENV === 'production') {
 	apiUrl = process.env.GOVT_SENT_API_URL || 'https://us-govt-sentiment-api.herokuapp.com';
 }
 
