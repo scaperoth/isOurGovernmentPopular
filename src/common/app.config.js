7b291ea928c8e1142837193763d6abce838de7d1
Matthew Scaperoth <scaperoth@gmail.com>
2019-08-07 12:17:38 -0500

    dynamically loading api url in lazy way for now

--- src/common/app.config.js
+++ src/common/app.config.js
@@ -1,3 +1,9 @@
 // app config with environment variables
 
-export const API_URL = process.env.GOVT_SENT_API_URL || 'http://localhost:5000';
+let apiUrl = process.env.GOVT_SENT_API_URL || 'http://localhost:5000';
+
+if(process.env.NODE_ENV === 'producution') {
+	apiUrl = process.env.GOVT_SENT_API_URL || 'https://us-govt-sentiment-api.herokuapp.com';
+}
+
+export const API_URL = apiUrl;
