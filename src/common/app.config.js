c72aac0a77b2fd7ca568c82d3d635a0470ddf9ea
Matthew Scaperoth <scaperoth@gmail.com>
2019-08-07 12:21:54 -0500

    adding console log for debugging

--- src/common/app.config.js
+++ src/common/app.config.js
@@ -1,7 +1,7 @@
 // app config with environment variables
 
 let apiUrl = process.env.GOVT_SENT_API_URL || 'http://localhost:5000';
-
+console.log('process.env', process.env);
 if(process.env.NODE_ENV === 'producution') {
 	apiUrl = process.env.GOVT_SENT_API_URL || 'https://us-govt-sentiment-api.herokuapp.com';
 }
